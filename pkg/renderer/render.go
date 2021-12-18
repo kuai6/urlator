a6a574b8ffe981348503a7983049854520867f6c
Aleskei I. Kuleshov <kuai6@ya.ru>
2021-12-18 20:28:24 +0300

    create renderer

--- pkg/renderer/render.go
+++ pkg/renderer/render.go
@@ -1,6 +1,12 @@
 package renderer
 
-import "github.com/kuai6/urlator/pkg/runner"
+import (
+	"sort"
+
+	"github.com/dustin/go-humanize"
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/kuai6/urlator/pkg/runner"
+)
 
 type renderer struct {
 }
@@ -9,6 +15,22 @@ func NewRenderer() *renderer {
 	return &renderer{}
 }
 
-func (r *renderer) Render([]runner.Result) string {
-	return ""
+func (r *renderer) Render(res []runner.Result) string {
+
+	sort.Slice(res, func(i, j int) bool {
+		return res[i].Size > res[j].Size
+	})
+
+	t := table.NewWriter()
+	t.AppendHeader(table.Row{"#", "URL", "Result"})
+	for i, row := range res {
+
+		value := humanize.Bytes(row.Size)
+		if row.Err != nil {
+			value = row.Err.Error()
+		}
+		t.AppendRow(table.Row{i, row.Url, value})
+	}
+
+	return t.Render()
 }
